afc06d8373435009e5df5ceed60f9921f7e5c390
Philipp Bosch <hello@pb.io>
2012-12-05 00:46:01 +0100

    Add compass watcher and output magnetic heading.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var jqt = new $.jQTouch();
 
 $(function() {
+    /* Gallery */
     $('#fotos').on('pageAnimationEnd', function() {
         var swipe = new Swipe(document.getElementById('gallery'));
     });
 
+
+    /* Concerts */
     $.getJSON('http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist=The+Rolling+Stones&api_key=b8505779457e587972157f50108505dd&format=json', function(data) {
         $.each(data.events.event, function(i, event) {
             var date = new Date(event.startDate);
@@ -12,6 +15,8 @@ $(function() {
         });
     });
 
+
+    /* Snapshot */
     function captureSuccess(files) {
         var ft = new FileTransfer();
         ft.upload(files[0].fullPath, 'http://pb.local:5000/upload/', uploadSuccess, uploadError);
@@ -32,4 +37,20 @@ $(function() {
     $('.button.snap').on('tap', function() {
         navigator.device.capture.captureImage(captureSuccess, captureError);
     });
+
+
+    /* Compass */
+    function compassSuccess(heading) {
+        $('#heading').text(heading.magneticHeading + '°');
+    }
+
+    function compassError(error) {
+        navigator.notification.alert('Compass error: ' + error.code);
+    }
+
+    function startCompass() {
+        navigator.compass.watchHeading(compassSuccess, compassError);
+    }
+
+    document.addEventListener('deviceready', startCompass, false);
 });
